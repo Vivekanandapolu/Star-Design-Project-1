0272cbabc555da17987a0a673dbc4a9674e14f70
vivkananda Reddy <vivekanandapolu123@gmail.com>
2024-04-16 16:57:12 +0530

    home component code pushed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ServicesComponent } from './components/services/services.component';
+// import { HeaderComponent } from './shared/header/header.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'services',
+    component: ServicesComponent
   }
+
 ];
 
 @NgModule({
