d2605d244810be3ad740a9c42feef4c4d562424f
vivkananda Reddy <vivekanandapolu123@gmail.com>
2024-04-16 22:16:49 +0530

    added real images for all sections

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  @ViewChild('cards') cardsContainers: ElementRef | undefined;
 
+  slide(direction: string) {
+    if (this.cardsContainers)
+      if (direction === 'left') {
+        this.cardsContainers.nativeElement.scrollLeft -= 350;
+      }
+      else if (direction === 'right') {
+        this.cardsContainers.nativeElement.scrollLeft += 350;
+      }
+  }
 }
