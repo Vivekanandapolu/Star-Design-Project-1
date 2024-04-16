9728ff7fce0665fa9c6c7f5af3c7cb79df95a7c1
vivkananda Reddy <vivekanandapolu123@gmail.com>
2024-04-17 00:39:20 +0530

    Changes in the icons of the why stra section and in the experience section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,19 @@
 
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  windowWidth: boolean = false
   @ViewChild('cards') cardsContainers: ElementRef | undefined;
+  ngOnInit(): void {
+    if (document.body.offsetWidth <= 1024) {
+      this.windowWidth = true
+    }
+  }
 
   slide(direction: string) {
     if (this.cardsContainers)
