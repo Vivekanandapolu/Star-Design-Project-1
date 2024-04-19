8c5eb99d062dbf8af1b4c387edb9145368cd9908
Vivekanandapolu <vivekanandapolu123@gmail.com>
2024-04-19 14:52:33 +0530

    placements

--- src/app/components/placements/placements.component.ts
+++ src/app/components/placements/placements.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-placements',
   templateUrl: './placements.component.html',
   styleUrls: ['./placements.component.scss']
 })
-export class PlacementsComponent {
+export class PlacementsComponent implements OnInit {
+  windowWidth: boolean = false
+  @ViewChild('cards') cardsContainers: ElementRef | undefined;
+  ngOnInit(): void {
+    this.windowWidth = window.innerWidth <= 1024;
+    window.addEventListener('resize', () => {
+      this.windowWidth = window.innerWidth <= 1024;
+    });
+  }
 
+
+  slide(direction: string) {
+    if (this.cardsContainers)
+      if (direction === 'left') {
+        this.cardsContainers.nativeElement.scrollLeft -= 350;
+      }
+      else if (direction === 'right') {
+        this.cardsContainers.nativeElement.scrollLeft += 350;
+      }
+  }
 }
