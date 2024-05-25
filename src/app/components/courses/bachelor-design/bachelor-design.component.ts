fb1e78d1e30105cb6b115af84ea31a753423dfa8
vivkananda Reddy <vivekanandapolu123@gmail.com>
2024-05-25 14:40:53 +0530

    latest changes in the pages

--- src/app/components/courses/bachelor-design/bachelor-design.component.ts
+++ src/app/components/courses/bachelor-design/bachelor-design.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-bachelor-design',
   templateUrl: './bachelor-design.component.html',
   styleUrls: ['./bachelor-design.component.scss']
 })
-export class BachelorDesignComponent {
-
+export class BachelorDesignComponent implements OnInit {
+  windowWidth: boolean = false;
+  ngOnInit(): void {
+    this.windowWidth = window.innerWidth <= 1024;
+    window.addEventListener('resize', () => {
+      this.windowWidth = window.innerWidth <= 1024;
+    });
+  }
 }
