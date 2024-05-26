20a28b5b855fff1bc1e312af2f48528bab878c6a
vivkananda Reddy <vivekanandapolu123@gmail.com>
2024-05-26 12:15:52 +0530

    small checnges

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.scss']
 })
-export class ServicesComponent {
+export class ServicesComponent implements OnInit {
+  windowWidth: number = window.innerWidth;
+  windowWidth786: boolean = false;
+
+  ngOnInit(): void {
+    this.windowWidth786 = window.innerWidth <= 768;
+    window.addEventListener('resize', () => {
+      this.windowWidth786 = window.innerWidth <= 768;
+      console.log(this.windowWidth786);
+    });
+  }
 
 }
